6349059ff03de2d40875c7da1bb047f878605ac9
ElenaButnik <butnik.elena@gmail.com>
2022-08-05 23:23:07 +0300

    server part

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { databaseConfig } from './config/configuration';
+import { SequelizeConfigService } from './config/sequelizeConfig.service';
+import { FormModule } from './form/form.module';
+
+@Module({
+  imports: [
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: SequelizeConfigService,
+    }),
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+    }),
+    FormModule,
+  ],
+})
+export class AppModule {}
